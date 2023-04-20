4fa91bf8df1785398f612d9fe69bc38f1ac307ff
salatiel6 <salatiel6@outlook.com>
2023-04-20 15:51:14 -0300

    REFACTOR: file_handler.py now can write files dynamically

--- src/controllers/file_handler.py
+++ src/controllers/file_handler.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import os
 
+
 class FileHandler:
     @staticmethod
     def reader(csv_file):
         module_dir = os.path.dirname(__file__)
 
         data_frame = pd.read_csv((os.path.join(
-        module_dir, '..', 'dataset', csv_file)))
+            module_dir, '..', 'dataset', csv_file)))
 
         return data_frame.to_dict(orient='records')
 
     @staticmethod
-    def writer(base_dict):
+    def writer(base_dict, fields, filename):
         module_dir = os.path.dirname(__file__)
 
         deliverables_dir = os.path.join(module_dir, '..', 'deliverables')
@@ -20,6 +21,6 @@ class FileHandler:
             os.makedirs(deliverables_dir)
 
         df = pd.DataFrame.from_dict(base_dict, orient='index')
-        df = df[['name', 'num_supported_bills', 'num_opposed_bills']]
+        df = df[fields]
         df.index.name = 'id'
-        df.to_csv((os.path.join(deliverables_dir, 'legislators-support-oppose-count.csv')))
+        df.to_csv((os.path.join(deliverables_dir, filename)))
